7f3918b4b5ee9fc2582cc625e0b5ca252b33e2ef
TImur <kamilyanov_timur@mail.ru>
2024-09-02 17:02:40 +0300

    Изменил логотип сайта, добавил static

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
 
