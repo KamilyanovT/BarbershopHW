7a6729bb833cfc077c3f45063b8a06e0b52b7e03
TImur <kamilyanov_timur@mail.ru>
2024-09-29 12:38:20 +0300

    HW52 откат коммита

--- core/signals.py
+++ core/signals.py
@@ -4,20 +4,18 @@ from django.dispatch import receiver
 from .models import Visit
 from .telegram_bot import send_telegram_message
 import asyncio
-from django.db.models.signals import m2m_changed
 
 
-@receiver(m2m_changed, sender=Visit.services.through)
-def send_telegram_notification(action, instance, **kwargs):
-    if action == "post_add" and kwargs.get("pk_set"):
-        services = [service.name for service in instance.services.all()]
+@receiver(post_save, sender=Visit)
+def send_telegram_notification(sender, instance, created, **kwargs):
+    if created:
         message = f"""
 *Новая запись на консультацию* 
 
 *Имя:* {instance.name} 
 *Телефон:* {instance.phone or 'не указан'} 
 *Мастер:* {instance.master}
-*Услуги:* {', '.join(services) or 'не указаны'}
+*Услуги:* {instance.services}
 -------------------------------------------------------------
 """
         asyncio.run(
